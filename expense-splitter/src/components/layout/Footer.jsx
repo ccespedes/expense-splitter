2f446f750e724a359270a7140040a4435e8595af
Carlos Cespedes <cespedes88@gmail.com>
2024-10-16 20:02:55 -0700

    Move nav to footer

--- expense-splitter/src/components/layout/Footer.jsx
+++ expense-splitter/src/components/layout/Footer.jsx
@@ -1,20 +1,31 @@
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import IconButton from "../ui/IconButton";
 
 const Footer = () => {
   const navigate = useNavigate();
 
   return (
-    <div
-      onClick={() => {
-        navigate("/about");
-      }}
-      className="over fixed bottom-0 left-1/2 z-40 flex w-full -translate-x-1/2 cursor-pointer bg-primary py-1 text-white"
-    >
-      <div className="mx-auto flex gap-2">
-        {" "}
-        <h2 className="text-center text-sm font-medium">About Our Team</h2>
+    <>
+      <div className="over fixed bottom-0 left-1/2 z-40 flex h-[100px] w-full -translate-x-1/2 flex-col rounded-t-3xl bg-white text-primary/40">
+        <nav className="m-auto w-full max-w-4xl px-4">
+          <div className="mb-6 flex justify-between gap-2">
+            <NavLink to="/home">
+              <IconButton icon={"house"}>Home</IconButton>
+            </NavLink>
+            <NavLink to="/groups">
+              <IconButton icon={"user-group"}>Groups</IconButton>
+            </NavLink>
+            <NavLink to="/friends">
+              <IconButton icon={"address-book"}>Friends</IconButton>
+            </NavLink>
+            <NavLink to="/expenses">
+              <IconButton icon={"credit-card"}>Expenses</IconButton>
+            </NavLink>
+          </div>
+        </nav>
       </div>
-    </div>
+      <div className="over pointer-events-none fixed bottom-0 left-1/2 z-10 flex w-full -translate-x-1/2 bg-gradient-to-t from-black/20 to-black/0 pb-28 pt-20"></div>
+    </>
   );
 };
 
