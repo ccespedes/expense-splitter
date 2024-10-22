3462c95f157ee6a267a3e6b00f613bccac8cfca7
Carlos Cespedes <cespedes88@gmail.com>
2024-10-22 16:35:55 -0700

    Add rounded corners to top of PlainSection

--- expense-splitter/src/components/layout/PlainSection.jsx
+++ expense-splitter/src/components/layout/PlainSection.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 // simple section with plain top
 const PlainSection = ({ children }) => {
   return (
     <div className="relative -top-[110px]">
       <div className="relative -ml-4 -mr-4">
-        <div className="absolute left-1/2 h-28 w-screen -translate-x-1/2 transform bg-gray-200"></div>
+        <div className="absolute left-1/2 h-28 w-full max-w-screen-lg -translate-x-1/2 transform rounded-t-2xl bg-gray-200"></div>
       </div>
       <div className="relative top-[20px]">{children}</div>
     </div>
