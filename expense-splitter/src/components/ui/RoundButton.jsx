31781e2b5c0092fad50475388e80ee998bf984d8
Carlos Cespedes <cespedes88@gmail.com>
2024-10-23 15:39:10 -0700

    Add onClick

--- expense-splitter/src/components/ui/RoundButton.jsx
+++ expense-splitter/src/components/ui/RoundButton.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const RoundButton = ({ children, className }) => {
+const RoundButton = ({ children, className, onClick }) => {
   return (
     <div
-      className={`${className} flex h-10 w-10 items-center justify-center rounded-full bg-white/10 backdrop-blur-sm`}
+      onClick={onClick}
+      className={`${className} flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white/10 backdrop-blur-sm`}
     >
       {/* <div className="h-8 w-8 rounded-full bg-white/20"></div> */}
       <div className="flex h-8 w-8 items-center justify-center">{children}</div>
