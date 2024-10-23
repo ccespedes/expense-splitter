a597015a0166352413f170df66b6378b63682515
Carlos Cespedes <cespedes88@gmail.com>
2024-10-22 21:22:18 -0700

    Add PlainSection to NotFound

--- expense-splitter/src/components/layout/NotFound.jsx
+++ expense-splitter/src/components/layout/NotFound.jsx
@@ -1,6 +1,8 @@
+import PlainSection from "./PlainSection";
+
 const NotFound = () => {
   return (
-    <>
+    <PlainSection>
       <h1 className="text-center">Page Not Found</h1>
       <p className="text-center">
         The link you followed may be broken or the page may have been
@@ -9,7 +11,7 @@ const NotFound = () => {
           <u>Go back to the home page.</u>
         </a>
       </p>
-    </>
+    </PlainSection>
   );
 };
 
